bc55221b88de95ba2499bc768234fb37d4c638f0
TsuyoshiOkada <106594828+Okaaada@users.noreply.github.com>
2022-06-20 08:02:35 +0900

    課題提出＿２回目

--- lesson5.js
+++ lesson5.js
@@ -1,18 +1,12 @@
-let count = getCount();
-
-if(count % 10 == 0){
-  console.log('今'+count+'回ループしました。');
-}else if(count % 4 == 0){
-  console.log('4で割り切れる数です。'+count);
-}
-if(count == 50) {
-  alert('50回カウントが終わりました。');
- }
-
-function getCount(){
-  let total = 1;
-  for(let i = 0; i < 51 ;i++){
-    total = i
+let count = 0;
+  for(let count = 0; count < 51 ;count++){
+    if(count % 10 == 0){
+      console.log('今'+count+'回ループしました。');
+    }
+    if(count % 4 == 0){
+      console.log('4で割り切れる数です。'+count);
+    }
+    if(count == 50) {
+      alert('50回カウントが終わりました。');
+     }
   }
-return total;
-}
